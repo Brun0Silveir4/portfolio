9248edba9ebb8fb9bb28c99b26b2bcd3c727d67d
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-05-29 23:45:54 -0300

    restarting code

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,21 @@
+import "./App.scss"
+
+
+
+export default function App(){
+   return (
+    <div className="home-content">
+        <div className="intro">
+            <div className="text-group">
+                <div className="title"></div>
+                <div className="resumed-bio"></div>
+                <div className="locale"></div>
+                <div className="social-media"></div>
+            </div>
+            <div className="image">
+                
+            </div>
+        </div>
+    </div>
+   )
+}
