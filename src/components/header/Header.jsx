0be8ccb50a83dadd5a094de56bb3b9848373e623
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-05-30 17:23:45 -0300

    add: header creation

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "./Header.scss";
+import { LuMoon } from "react-icons/lu";
+import teste from "../../assets/CV-Bruno-Silveira-Dionisio.pdf"
+
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const onButtonClick = () => {
+    fetch(teste).then((response) => {
+        response.blob().then((blob) => {
+            const fileURL = window.URL.createObjectURL(blob);
+            let alink = document.createElement("a");
+            alink.href = fileURL;
+            alink.download = "CV Bruno Silveira.pdf";
+            alink.click();
+        });
+    });
+};
+
+  return (
+    <div className={`header ${isScrolled ? "transparent" : ""}`}>
+      <div className="header-items">
+        <div className="navigate">
+          <p>Sobre</p>
+          <p>Projetos</p>
+          <p>Contato</p>
+        </div>
+        <div className="btn-group">
+          <LuMoon size={24} className="icon" />
+          <button onClick={onButtonClick}>Download CV</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,21 +1,24 @@
-import "./App.scss"
+import "./App.scss";
+import { useState, useEffect } from "react";
+
+import Header from "../../components/header/Header";
+
+export default function App() {
 
 
 
-export default function App(){
-   return (
+  return (
     <div className="home-content">
-        <div className="intro">
-            <div className="text-group">
-                <div className="title"></div>
-                <div className="resumed-bio"></div>
-                <div className="locale"></div>
-                <div className="social-media"></div>
-            </div>
-            <div className="image">
-                
-            </div>
+     <Header />
+      <div className="intro">
+        <div className="text-group">
+          <div className="title"></div>
+          <div className="resumed-bio"></div>
+          <div className="locale"></div>
+          <div className="social-media"></div>
         </div>
+        <div className="image"></div>
+      </div>
     </div>
-   )
+  );
 }
