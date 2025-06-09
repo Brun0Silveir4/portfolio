283784655ff8ec5359b84e937a3d9c55f1dc479a
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-09 11:41:20 -0300

    add: router configuration

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import React from "react"
+import router from "./router"
+import { RouterProvider } from "react-router-dom";
+
+export default function App() {
+  return < RouterProvider router={router}/>
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import "./App.scss";
+import "./Home.scss";
 
 import Header from "../../components/items/header/Header";
 import TextHome from "../../components/items/text-home/Text-home";
 import Technologies from "../../components/items/technologies/Technologies";
 
-export default function App() {
+export default function Home() {
   return (
     <div className="home-content">
       <Header />
