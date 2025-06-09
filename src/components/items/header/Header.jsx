383501d19f3cdcb81cbcdec0c9bf087d7cbec4c1
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-09 12:02:43 -0300

    add: pre config routes in header

--- src/components/items/header/Header.jsx
+++ src/components/items/header/Header.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import "./Header.scss";
 import { LuMoon } from "react-icons/lu";
-import CV from "../../../../public/docs/CV-Bruno-Silveira-Dionisio.pdf"
+import CV from "../../../../public/docs/CV-Bruno-Silveira-Dionisio.pdf";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const location = useLocation();
+  const pathname = location.pathname;
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,24 +22,32 @@ export default function Header() {
 
   const onButtonClick = () => {
     fetch(CV).then((response) => {
-        response.blob().then((blob) => {
-            const fileURL = window.URL.createObjectURL(blob);
-            let alink = document.createElement("a");
-            alink.href = fileURL;
-            alink.download = "CV Bruno Silveira.pdf";
-            alink.click();
-        });
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "CV Bruno Silveira.pdf";
+        alink.click();
+      });
     });
-};
+  };
 
   return (
     <div className={`header ${isScrolled ? "transparent" : ""}`}>
       <div className="header-items">
         <div className="navigate">
-          <p>Home</p>
-          <p>Sobre</p>
-          <p>Projetos</p>
-          <p>Contato</p>
+          <Link to="/" className="link-css">
+            <p className={pathname === "/" ? "active" : ""}>Home</p>
+          </Link>
+          <Link to="/" className="link-css">
+            <p className={pathname === "/about" ? "active" : ""}>Sobre</p>
+          </Link>
+          <Link to="/" className="link-css">
+            <p className={pathname === "/project" ? "active" : ""}>Projetos</p>
+          </Link>
+          <Link to="/" className="link-css">
+            <p className={pathname === "/contact" ? "active" : ""}>Contato</p>
+          </Link>
         </div>
         <div className="btn-group">
           <LuMoon size={24} className="icon" />
