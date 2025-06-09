b00c2f5106785fd190795143f732303d8a942f10
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-09 11:26:14 -0300

    update: add target blank on tech component and update transition style

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -3,7 +3,7 @@ import "./Tech.scss";
 export default function Tech({ doc, url, text, style }) {
   return (
     <div className="tech">
-      <a href={doc}>
+      <a href={doc} target="_blank">
         <img src={url} style={style} />
         <p>{text}</p>
       </a>
