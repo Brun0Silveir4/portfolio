b06c3c0e51fe9dc0016a6ce1be7897b3d0a5801b
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-09 23:09:47 -0300

    add: starting creating first card of exp

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,6 +11,25 @@ export default function About() {
     <div className="about-container">
       <Header />
       <AboutContent />
+      <div className="experiences">
+        <div className="experiences-title">
+          <p>Experiências</p>
+        </div>
+        <div className="card-exp-group">
+          <div className="card-exp">
+            <div className="header-card">
+              <div className="img-header-exp">
+                
+              </div>
+              <div className="company-exp"></div>
+              <div className="time-exp"></div>
+            </div>
+            <div className="exp-content">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
