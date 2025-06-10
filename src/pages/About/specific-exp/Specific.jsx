8fc5d257b7c440fa32af4b865353c8670b4ed328
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-10 17:26:13 -0300

    update: finishing specific page content

--- src/pages/About/specific-exp/Specific.jsx
+++ src/pages/About/specific-exp/Specific.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import texts from "../../../assets/Experiences.json";
 import Header from "../../../components/items/header/Header";
+import { BiArrowBack } from "react-icons/bi";
 
 export default function Specific() {
   const { experience } = useParams();
@@ -21,11 +22,26 @@ export default function Specific() {
   if (!exp) return null;
 
   return (
-    <div>
+    <div className="container-specific">
       <Header />
-      {exp.experiences.map((expe, id) => (
-        <p key={id}>{expe}</p>
-      ))}
+      <div className="data-specific">
+        <div className="back">
+        <BiArrowBack size={25} style={{cursor: 'pointer'}} onClick={() => navigate('/about')}/>
+        </div>
+        <div className="title-specific">
+          <p>{exp.company.toUpperCase()}</p>
+        </div>
+        <div className="main-content-specific">
+          <div className="image-specific">
+            <img src={exp.url_image} alt="" className="img-card" />
+          </div>
+          <div className="resume">
+            {exp.experiences.map((para, id) => (
+              <p key={id}>{para}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
