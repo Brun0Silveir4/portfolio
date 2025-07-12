85ad589ff8373b5d5b2f0a00f92a20554a29a023
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-07-11 22:43:59 -0300

    fix: fix projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -31,42 +31,6 @@ export default function Projects() {
       clicable: false,
       liveLink: '',
       repo: ''
-    },
-    {
-      title: "Asset Aphrodite - IBM",
-      description: "Asset com interface replicável e adaptável ao cliente, com foco em exibir um chat criado no WatsonX Orchestrate.",
-      image: Aphrodite,
-      techs: ["react", "orchestrate", "carbon", 'sass'],
-      clicable: false,
-      liveLink: '',
-      repo: ''
-    },
-    {
-      title: "Asset Aphrodite - IBM",
-      description: "Asset com interface replicável e adaptável ao cliente, com foco em exibir um chat criado no WatsonX Orchestrate.",
-      image: Aphrodite,
-      techs: ["react", "orchestrate", "carbon", 'sass'],
-      clicable: false,
-      liveLink: '',
-      repo: ''
-    },
-    {
-      title: "Asset Aphrodite - IBM",
-      description: "Asset com interface replicável e adaptável ao cliente, com foco em exibir um chat criado no WatsonX Orchestrate.",
-      image: Aphrodite,
-      techs: ["react", "orchestrate", "carbon", 'sass'],
-      clicable: false,
-      liveLink: '',
-      repo: ''
-    },
-    {
-      title: "Asset Aphrodite - IBM",
-      description: "Asset com interface replicável e adaptável ao cliente, com foco em exibir um chat criado no WatsonX Orchestrate.",
-      image: Aphrodite,
-      techs: ["react", "orchestrate", "carbon", 'sass'],
-      clicable: false,
-      liveLink: '',
-      repo: ''
     }
   ];
 
